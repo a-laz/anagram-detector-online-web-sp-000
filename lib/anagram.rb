7eb4200f9c9c378485f8d93477341a5de68a67b5
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-25 01:39:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,16 @@ class Anagram
     @word = word
   end
 
-  def match
+  def match(words)
+    word_array_for_word = @word.split("")
+    matches = []
+    words.each |word|
+      word_array = word.split("")
+      if word_array.sort == word_array_for_word.sort
+        matches << word
+      end
+    end
+    matches
   end
 
 
